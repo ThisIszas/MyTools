9f2041383014972738e29a587968ab322634d1a1
zhengLi <a838502550@gmail.com>
2020-03-30 15:53:18 +0800

    text field can merge by ordered list

--- ExcelRowMerge/MergeConfig.py
+++ ExcelRowMerge/MergeConfig.py
@@ -1,18 +1,23 @@
-# Available values for 'WhichValue' properties: ['FirstValue', 'LastValue', 'Biggest', 'Smallest', 'Empty', 'Average']
+# Available values for 'WhichValue' properties: ['FirstValue', 'LastValue', 'Biggest', 'Smallest', 'Average']
 '''
-    Empty, FirstValue, LastValue 通用
+    FirstValue, LastValue 通用
     CheckBox:
         Data示例: TRUE, FALSE, 1, 0, YES, NO
-        WhichValue 限制: 'Biggest', 'Smallest'
+        WhichValue Limit: 'Biggest', 'Smallest'
     Date:
         Date示例: 2020-02-02 只接受YYYY-MM-DD格式
-        WhichValue 限制: 'Biggest', 'Smallest'
+        WhichValue Limit: 'Biggest', 'Smallest'
     Number:
-        Date示例: 123456
-        WhichValue 限制: 'Biggest', 'Smallest', 'Average': todo
+        Number示例: 123456
+        WhichValue Limit: 'Biggest', 'Smallest', 'Average': todo
+    Text:
+        Text示例: 'fsfsdf', '发生的发生', '23433434343'
+        WhichValue Limit: 'OrderedList'
+        OrderedList: ['2', '3', '4']   // Required for 'OrderedList' type only, omit this field if type is others.
+        FieldName: columnName   // Required for 'OrderedList' type only, omit this field if type is others.
     All:
         Value示例: whatever you want
-        WhichValue 限制: 'Empty', 'FirstValue', 'LastValue'
+        WhichValue Limit: 'FirstValue', 'LastValue'
     ---------------------------------------------------------------------------------------
     todo:
         Average 这个类型没做. 看心情....
@@ -58,7 +63,13 @@ RowMergeMap = {
         'Handover_Date__c': {
             'FieldType': 'Date',
             'WhichValue': 'Biggest'
-        }
+        },
+        'StageName': {
+            'FieldType': 'Text',
+            'WhichValue': 'OrderedList',
+            'OrderedList': ['Handover', 'Order Taken', 'Offer/Quotation'],
+            'FieldName': 'StageName'
+        },
     },
 
     'DataPreConvertMap': {
